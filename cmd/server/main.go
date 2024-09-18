1382fc8734371c2cb5f7ebd86a4c669aa611a763
Piotr Kaczmarski <piotr.kaczmarski.ext@assetmax.ch>
2024-09-18 12:50:58 +0200

    Ch 2 - 4: Connect

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connectionString)
+	if err != nil {
+		fmt.Println("Error connecting to RabbitMQ:", err)
+		return
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println("Error creating channel:", err)
+		return
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"peril_topic", // name
+		false,         // durable
+		false,         // delete when unused
+		false,         // exclusive
+		false,         // no-wait
+		nil,           // arguments
+	)
+	if err != nil {
+		fmt.Println("Error declaring queue:", err)
+		return
+	}
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		false,  // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	if err != nil {
+		fmt.Println("Error consuming:", err)
+		return
+	}
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			fmt.Printf("Received a message: %s\n", d.Body)
+		}
+	}()
+
+	fmt.Println("Waiting for messages. To exit press CTRL+C")
+	<-forever
 }
